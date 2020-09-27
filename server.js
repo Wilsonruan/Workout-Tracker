245672e2235c932ea9b5aa1447be1a5c3ff9bf9b
Wilson Ruan <wilsonruan23@gmail.com>
2020-09-26 22:27:58 -0400

    inits server and model

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+
+// routes
+app.use(require("./seeders/seed.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
