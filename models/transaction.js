b8aa95ca703465de799adecf45ab6d74d193b6cc
Wilson Ruan <wilsonruan23@gmail.com>
2020-09-27 21:33:19 -0400

    inits transaction

--- models/transaction.js
+++ models/transaction.js
@@ -3,20 +3,56 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema({
-    // name: {
-    //   type: String,
-    //   trim: true,
-    //   required: "Enter a name for transaction"
-    // },
-    // value: {
-    //   type: Number,
-    //   required: "Enter an amount"
-    // },
-    // date: {
-    //   type: Date,
-    //   default: Date.now
-    // }
-  });
+  day: {
+      type: Date,
+      default: () => new Date()
+  },
+  exercises: [
+      {
+          type: {
+              type: String,
+              trim: true,
+              required: "Please enter an exercise type"
+          },
+          name: {
+              type: String,
+              trim: true,
+              required: "Please enter an exercise name"
+          },
+          duration: {
+              type: Number,
+              trim: true,
+              required: "Please enter a duration of workout"
+          },
+          weight: {
+              type: Number,
+              trim: true
+          },
+          reps: {
+              type: Number,
+              trim: true
+          },
+          sets: {
+              type: Number,
+              trim: true
+          },
+          distance: {
+              type: Number,
+              trim: true
+          }
+      }
+  ]
+},
+{
+  toJSON:{virtuals: true}
+}
+);
+
+transactionSchema.virtual("totalDuration").get( function() {
+  return this.exercises.reduce((total, exercise) => {
+      return total+exercise.duration;
+  }, 0)
+});
   
   const Transaction = mongoose.model("Transaction", transactionSchema);
   
