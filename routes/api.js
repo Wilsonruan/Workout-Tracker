74ba79987dd39961e020dd1a96a7a97d2e718026
Wilson Ruan <wilsonruan23@gmail.com>
2020-09-28 21:46:19 -0400

    Fix bugs

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercist.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 router.get("/api/workouts", (req, res) => {
