c27c822441f131f61eb1c5abcd7aef431c94b8f9
Wilson Ruan <wilsonruan23@gmail.com>
2020-09-28 22:01:46 -0400

    Fix join to json

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ router.get("/stats", (req, res) => {
 router.get("/api/workouts", (req, res) => {
     Workout.find({})
     .then(dbWorkout => {
-        res.join(dbWorkout);
+        res.json(dbWorkout);
     })
 });
 
@@ -21,21 +21,21 @@ router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
     .sort({ day: -1})
     .then(dbWorkout => {
-        res.join(dbWorkout)
+        res.json(dbWorkout)
     })
 })
 
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
-        res.join(dbWorkout);
+        res.json(dbWorkout);
     });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body }})
     .then(dbWorkout => {
-        res.join(dbWorkout);
+        res.json(dbWorkout);
     })
 })
 
